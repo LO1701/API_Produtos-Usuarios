1a992ddf5f0bb0c3ee1a6dbfed2cd4de3e85a12f
Lurian Ortiz <lurian502@gmail.com>
2022-12-16 10:33:56 -0300

    Alterações no UsuarioController

--- controller/UsuarioController.js
+++ controller/UsuarioController.js
@@ -4,21 +4,92 @@ import argon2 from "argon2";
 
 export const getUsuario = async (req, res) => {
     try{
-        const response = await Usuario.findAll();
+        const response = await Usuario.findAll({
+            attributes:['uuid', 'nome', 'email', 'role']
+        });
         res.status(200).json(response);
-    }catch{
+    }catch(error){
         res.status(500).json({msg: error.message});
     }
 }
 
-export const postUsuario = (req, res) => {
+export const postUsuario = async (req, res) => {
+    const {nome, email, senha, confirmaSenha, role} = req.body;
 
+    if(senha !== confirmaSenha)
+        return res.status(400).json({msg: "Senha diferente da confirmação"});
+    
+    const hashSenha = await argon2.hash(senha);        
+    
+    try{
+        await Usuario.create({
+            nome: nome,
+            email: email,
+            senha: hashSenha,
+            role: role
+        });
+        res.status(201).json({msg: "Usuario criado com sucesso"});
+    }catch(error){
+        res.status(500).json({msg: error.message});
+    }
 }
 
-export const patchUsuario = (req, res) => {
+export const patchUsuario = async (req, res) => {
+    const usuario = await Usuario.findOne({
+        where: {
+            uuid: req.params.id
+        }
+    });
+    
+    if(!usuario)
+        return res.status(404).json({msg: "Usuario não encontrado"});
 
+    const  {nome, email, senha, confirmaSenha, role} = req.body;
+    let hashSenha;
+    
+    if(senha === "" || senha === null){
+        hashSenha = usuario.senha; 
+    }else{
+        hashSenha = await argon2.hash(senha);
+    }
+
+    if(senha !== confirmaSenha)
+        return res.status(400).json({msg: "Senha diferente da confirmação"})
+    try{
+        await Usuario.update({
+            nome: nome,
+            email: email,
+            senha: hashSenha,
+            role: role
+        },{
+            where:{
+                id: usuario.id
+            }
+        });
+        res.status(200).json({msg: "Usuario alterado com sucesso"});
+    }catch(error){
+        res.status(400).json({msg:error.message});
+    }
 }
 
-export const deleteUsuario = (req, res) => {
+export const deleteUsuario = async (req, res) => {
+    const user = await Usuario.findOne({
+        where: {
+            uuid: req.params.id
+        }
+    });
+    
+    if(!user)
+        return res.status(404).json({msg: "Usuario não encontrado"});
 
+    try{
+        await Usuario.destroy({
+            where:{
+                id: user.id
+            }
+        });
+        res.status(200).json({msg: "Usuario apagado com sucesso"});
+    }catch(error){
+        res.status(400).json({msg:error.message});
+    }
 }
