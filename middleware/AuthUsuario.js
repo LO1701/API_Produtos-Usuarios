067b71a3d6f0cc7d5c653c3a95d52815de6013a8
Lurian Ortiz <lurian502@gmail.com>
2023-02-08 11:05:17 -0300

    Alterações no AuthUsuario

--- middleware/AuthUsuario.js
+++ middleware/AuthUsuario.js
@@ -2,13 +2,14 @@ import Usuario from "../model/UsuarioModel.js";
 
 export const vereficarUsuario = async (req, res, next) => {
     if(!req.session.userId)
-    return res.status(401).json({msg: "Faça o login"});
+        return res.status(401).json({msg: "Faça o login"});
 
     const usuario = await Usuario.findOne({
         where: {
             uuid: req.session.userId
         }
     });
+    
     if(!usuario) 
         return res.status(404).json({msg: "Usuario não encontrado"});
 
